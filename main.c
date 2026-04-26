3f5f475eace2c8e607a0d3e2866c6273119a38db
Roy Ratcliffe <roy@ratcliffe.me>
2026-04-26 12:50:30 +0100

    Refactor main function structure in main.c
    Reorganise includes and formatting in the main function for
    improved readability. Maintain functionality while enhancing
    code structure.

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 
+int main() {
+  stdio_init_all();
 
-int main()
-{
-    stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+  while (true) {
+    printf("Hello, world!\n");
+    sleep_ms(1000);
+  }
 }
