6ca14d8c02a257ba880fe86536d029d5ba834eee
Roy Ratcliffe <roy@ratcliffe.me>
2026-04-26 13:16:59 +0100

    Start FreeRTOS scheduler in main function
    Replace the infinite loop with a call to vTaskStartScheduler,
    allowing FreeRTOS to manage task execution.

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 int main() {
   stdio_init_all();
 
-  while (true) {
-    printf("Hello, world!\n");
-    sleep_ms(1000);
-  }
+  /*
+   * Start the FreeRTOS scheduler. The main function will not continue
+   * past this point, as the scheduler will take over and run the tasks.
+   * Add tasks and pre-start functionality before launching the
+   * scheduler.
+   */
+  vTaskStartScheduler();
 }
